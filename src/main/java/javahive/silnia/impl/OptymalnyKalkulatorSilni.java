d372496ba44adb8b7785d0d5dbc865243a430ff8
KamilOchnio <akamil47@gmail.com>
2014-04-09 14:17:15 +0200

    Update OptymalnyKalkulatorSilni.java

--- src/main/java/javahive/silnia/impl/OptymalnyKalkulatorSilni.java
+++ src/main/java/javahive/silnia/impl/OptymalnyKalkulatorSilni.java
@@ -2,17 +2,21 @@ package javahive.silnia.impl;
 
 import javahive.silnia.KalkulatorSilni;
 
-
-
-
 public class OptymalnyKalkulatorSilni implements KalkulatorSilni{
     
-    private static int MAKSYMALNY_ARGUMENT_DLA_LONG=12;
+    private static final int MAKSYMALNY_ARGUMENT_DLA_LONG=12;
     
     public String licz(int podstawa){
-        KalkulatorSilni i=null;
-        //w zależności od rozmiaru argumentu powinniśmy użyć algorytmu SzybkaSilnia lub BigDecimalSilnia
-        //TODO - zaimplementować to tutaj.
-        return i.licz(podstawa);
+        KalkulatorSilni kalkulatorSilni=null;
+        if(podstawa<0){
+            throw new ArithmeticException(
+                    "Nie mozna obliczyc silni z liczby ujemnej");
+        }
+        if(podstawa<=MAKSYMALNY_ARGUMENT_DLA_LONG){
+            kalkulatorSilni=new SzybkiKalkulatorSilni();
+        }else{
+            kalkulatorSilni=new KalkulatorSilniBigDecimal();
+        }
+        return kalkulatorSilni.licz(podstawa);
     }
 }
