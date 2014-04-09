2c0ed87a44a11b7c12b49962407d657e49301c4a
KamilOchnio <akamil47@gmail.com>
2014-04-09 14:15:47 +0200

    Update KalkulatorSilniBigDecimal.java

--- src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniBigDecimal.java
@@ -1,34 +1,33 @@
 package javahive.silnia.impl;
 
-
 import java.math.BigDecimal;
 
 import javahive.silnia.KalkulatorSilni;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
- *
+ * 
  * @author Marcin Grabowiecki
  * 
- * proszę zaimplementować algorytm analogiczny jak dla SzybkaSilnia
- * jednak należy się oprzeć na na typie BigDecimal
+ *         proszę zaimplementować algorytm analogiczny jak dla SzybkaSilnia
+ *         jednak należy się oprzeć na na typie BigDecimal
  * 
  */
 
 public class KalkulatorSilniBigDecimal implements KalkulatorSilni{
-    //metoda licząca silnie w sposób rekurencyjny
-    public BigDecimal liczBigDecimal(long podstawa){
-    	if(podstawa<0) throw new ArithmeticException();
-        if(podstawa>0) {
-            return liczBigDecimal(podstawa-1).multiply(BigDecimal.valueOf(podstawa));
-        } else {
-            return BigDecimal.ONE;
+    
+    private BigDecimal liczBigDec(int arg){
+        if(arg<0){
+            throw new ArithmeticException(
+                    "Nie mozna obliczyc silni z liczby ujemnej");
+        }
+        if(arg>0){
+            return BigDecimal.valueOf(arg).multiply(liczBigDec(arg-1));
+        }else{
+            return BigDecimal.valueOf(1);
         }
     }
     
-    public String licz(int arg) {
-        //wynik działania rekrurencyjnej metody rzutowany jest na String,
-        //Bo string przechowa bez problemu duże liczby
-        return ""+liczBigDecimal(arg);
+    public String licz(int arg){
+        return liczBigDec(arg).toString();
     }
 }
