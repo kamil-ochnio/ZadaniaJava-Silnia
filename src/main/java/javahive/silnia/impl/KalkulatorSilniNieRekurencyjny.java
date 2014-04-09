aceb90a86d560bc15c8ee97c256a6609b17bcd8f
KamilOchnio <akamil47@gmail.com>
2014-04-09 14:16:54 +0200

    Update KalkulatorSilniNieRekurencyjny.java

--- src/main/java/javahive/silnia/impl/KalkulatorSilniNieRekurencyjny.java
+++ src/main/java/javahive/silnia/impl/KalkulatorSilniNieRekurencyjny.java
@@ -1,30 +1,29 @@
 package javahive.silnia.impl;
 
 import java.math.BigDecimal;
-
 import javahive.silnia.KalkulatorSilni;
 
 /**
- *
+ * 
  * @author Marcin Grabowiecki
  */
-public class KalkulatorSilniNieRekurencyjny implements KalkulatorSilni {
-
-    public BigDecimal liczB(int podstawa) {
-        if (podstawa < 0) {
-            throw new ArithmeticException("podstawa silni nie może być mniejsza od zera");
+public class KalkulatorSilniNieRekurencyjny implements KalkulatorSilni{
+    
+    private BigDecimal liczB(int podstawa){
+        if(podstawa<0){
+            throw new ArithmeticException("Nie mozna obliczyc silni z liczby ujemnej");
         }
-        BigDecimal wynik = new BigDecimal(1);
-        if (podstawa > 0) {
-            for (int i = 1; i <= podstawa; i++) {
-                wynik = wynik.multiply(BigDecimal.valueOf(i));
+        BigDecimal wynik=BigDecimal.valueOf(1);
+        if(podstawa>0){
+            for(int i=1;i<=podstawa;i++){
+                wynik=wynik.multiply(BigDecimal.valueOf(i));
             }
             return wynik;
         }
         return BigDecimal.ONE;
     }
-
-    public String licz(int arg) {
+    
+    public String licz(int arg){
         return liczB(arg).toString();
     }
 }
