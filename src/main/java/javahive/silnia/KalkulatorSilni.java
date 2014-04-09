67832a3749929f6380b7ff66cc80cc7fce4a7488
KamilOchnio <akamil47@gmail.com>
2014-04-09 14:20:56 +0200

    Update KalkulatorSilni.java

--- src/main/java/javahive/silnia/KalkulatorSilni.java
+++ src/main/java/javahive/silnia/KalkulatorSilni.java
@@ -3,11 +3,13 @@
  * and open the template in the editor.
  */
 package javahive.silnia;
+
 /**
  * nowa uwaga - test gita
+ * 
  * @author Marcin Grabowiecki
  */
 
-public interface KalkulatorSilni {
-    public String licz(int arg) throws ArithmeticException;
+public interface KalkulatorSilni{
+    String licz(int arg);
 }
